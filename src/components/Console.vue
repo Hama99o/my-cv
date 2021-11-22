68b44aa0b15b7663a1912f9e3a071461aafa2b20
hama <hmmshl@gmail.com>
2021-11-22 23:14:52 +0100

    console text updated

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -3,13 +3,12 @@
     <div class="top"> <span class="options">⦿ ○ ○</span> <span class="title">Hama99o - Web developer </span></div>
     <div class="text">
       <span class="pink">root@hama99o:~$ </span> rails c
-      <br> Running via Spring preloader in process 50056 Loading development environment (Rails 6.1.4) irb: warn: can't alias context from irb_context.
       <br> 2.5.7 :001 > <span class="text-success">my_name, my_age </span> <span class="text-primary">= </span> <span class="orange">'Hammayoun Safi',</span> <span class="text-warning">{{myAge}}</span>
       <br> 2.5.7 :002 > <span class="text-success">from </span> <span class="text-primary">= </span> <span class="orange">'Afghanistan'</span>
       <br> 2.5.7 :003 > <span class="text-success">lived </span> <span class="text-primary">= </span> <span class="orange">['Sweden', 'Italy']</span>
       <br> 2.5.7 :004 > <span class="text-success">living </span> <span class="text-primary">= </span> <span class="orange"> 'Malakoff, France'</span>
       <br> 2.5.7 :005 > <span class="text-success">languages </span> <span class="text-primary">= </span> <span class="orange">['pashto', 'french' 'english', 'swedish', 'urdu', 'hindi', 'persian']</span>
-      <br> 2.5.7 :006 > <span class="text-success">my_hobbies</span> <span class="text-primary">= </span> <span class="orange">[coding, travel, hiking, snowboarding, swimming, cycling]</span>
+      <br> 2.5.7 :006 > <span class="text-success">my_hobbies</span> <span class="text-primary">= </span> <span class="orange">['coding', 'travel', 'hiking', 'snowboarding', 'swimming', 'cycling']</span>
       <br> 2.5.7 :007 > <input type="text" class="text-success console-input" autofocus>
     </div>
   </div>
