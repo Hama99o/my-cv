3dc9888d0902472bf98e83cb02a92fb59d0dee05
hama <hmmshl@gmail.com>
2021-11-23 18:27:03 +0100

    vuex added

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import Vuex from "vuex";
 
+Vue.use(Vuex);
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
