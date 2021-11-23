3e51306be203520d65de8aa851311dcd795897cb
hama <hmmshl@gmail.com>
2021-11-23 20:17:00 +0100

    new method added

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,15 @@ const getters = {
   toggleTextColorForNightMood ()  {
     return state.hasNightMood ? 'text-white' : 'text-dark'
   },
+  bgnightmodeClass () {
+    return state.hasNightMood ? 'bg-white  text-dark' : 'bg-dark text-white'
+  },
+  textClass () {
+    return state.hasNightMood ? 'text-dark' : 'text-white'
+  },
+  textAndBgSameClass () {
+    return state.hasNightMood ? 'bg-white text-white' : 'bg-dark text-dark'
+  }
 }
 
 const mutations = {
