3a60a5ba231e5b2eac3795ea2ed0f7f00c8eaf72
hama <hmmshl@gmail.com>
2021-11-23 20:32:31 +0100

    contect updated

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
   <div>
     <b-list-group>
       <div id="contact-form" class="contact-form">
-        <h1 class="contact-form_title" :class="toggleDarkTextColorForNightMood()">Contact Form</h1>
+        <h1 class="contact-form_title" :class="toggleWhiteTextColorForNightMood">Contact Form</h1>
         <div class="separator"></div>
 
         <div v-if="isSending" class="loading">Sendig...</div>
@@ -11,7 +11,7 @@
           <input required name="name" v-model='contact.name' placeholder="Name" type="text" autocomplete="off">
           <input required name="email" v-model="contact.email" placeholder="E-mail" type="email" autocomplete="off">
           <textarea name="message" v-model="contact.message" rows="4" placeholder="Message"></textarea>
-           <button class="button" :class="toggleWhitebgAndDarkTextClass()">Send</button>
+           <button class="button" :class="toggleWhitebgAndDarkTextClass">Send</button>
         </form>
       </div>
     </b-list-group>
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'Contact',
@@ -33,17 +34,9 @@ export default {
     }
 	},
   computed: {
-    nightmode () {
-      return this.$store.state.hasNightMood
-    }
+    ...mapGetters(['toggleWhitebgAndDarkTextClass', 'toggleWhiteTextColorForNightMood'])
   },
 	methods: {
-    toggleWhitebgAndDarkTextClass () {
-      return this.nightmode ? 'bg-white  text-dark' : 'bg-dark text-white'
-    },
-    toggleDarkTextColorForNightMood () {
-      return this.nightmode ? 'text-white' : 'text-dark'
-    },
 		clearForm() {
 			for (let field in this.contact) {
 				this.contact[field] = ''
