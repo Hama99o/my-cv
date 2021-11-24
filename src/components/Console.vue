efbee546f6a16e5d68fb364b881b277a44c917b3
hama <hmmshl@gmail.com>
2021-11-24 22:53:33 +0100

    naming updated

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -13,7 +13,7 @@
       <br> 2.5.7 :008 > <span class="text-success">web_technologies </span> <span class="text-primary">= </span> <span class="orange">['HTML','CSS']</span>
       <br> 2.5.7 :009 > <span class="text-success">database_server </span> <span class="text-primary">= </span> <span class="orange">['PostgreSQL']</span>
       <br> 2.5.7 :010 > <span class="text-success">frameworks </span> <span class="text-primary">= </span> <span class="orange">['Ruby on Rails','VueJS','NodeJS(express)','Bootstrap']</span>
-      <br> 2.5.7 :011 > <span class="text-success">other_dev_tool </span> <span class="text-primary">= </span> <span class="orange">['Git','Gitlab','Heroku','Linux']</span>
+      <br> 2.5.7 :011 > <span class="text-success">other_dev_tool </span> <span class="text-primary">= </span> <span class="orange">['Git','Github','Heroku','Linux']</span>
       <br> 2.5.7 :012 > <span class="text-success">exit</span>
       <br> 2.5.7 :013 > <input type="text" class="text-success console-input w-80" autofocus>
     </div>
