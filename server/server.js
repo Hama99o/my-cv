dfeac511c8889e0d85e0c487f9c3e9fab6674c6a
hama <hmmshl@gmail.com>
2021-11-26 22:26:02 +0100

    server updated

--- server/server.js
+++ server/server.js
@@ -1,27 +1,15 @@
 const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-// const serveStatic = require('serve-static')
-// var history = require('connect-history-api-fallback')
-// const path = require('path')
+const serveStatic = require('serve-static')
+const path = require('path')
+var history = require('connect-history-api-fallback')
+const port = process.env.PORT || 80
 
 const app = express()
 
-// app.use(history())
-// app.use('/', serveStatic(path.join(__dirname, 'dist')))
+// Static frontend
+app.use(history())
+app.use('/', serveStatic(path.join(__dirname, 'dist')))
 
-app.use(morgan('tiny'))
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Behold The MEVN Stack!'
-  })
-})
-
-const port = process.env.PORT || 4000
 app.listen(port, () => {
-  console.log(`listening on ${port}`)
+  console.log('App is running on port ' + port)
 })
