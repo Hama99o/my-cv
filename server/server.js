4813f449b83f3a3a6fcf25474685314fe3bd8148
hama <hmmshl@gmail.com>
2021-11-27 00:17:49 +0100

    server js updated

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+var history = require('connect-history-api-fallback')
 const port = process.env.PORT || 8081
 
 const app = express()
+
+// Static frontend
+app.use(history())
 app.use('/', serveStatic(path.join(__dirname, '..', 'dist')))
 
 app.listen(port, () => {
