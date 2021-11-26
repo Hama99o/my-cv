e6e00742c7d435bc274b78a9bc2e2ef19c09c1e2
hama <hmmshl@gmail.com>
2021-11-26 23:06:45 +0100

    port updated

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 
 const app = express()
 app.use('/', serveStatic(path.join(__dirname, 'dist')))
