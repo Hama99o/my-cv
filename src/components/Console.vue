a8551d1204e58dc8bbd564dc4e25459900c574b7
hama <hmmshl@gmail.com>
2021-11-26 14:08:01 +0100

    console updated

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -15,7 +15,7 @@
       <br> 2.5.7 :010 > <span class="text-success">frameworks </span> <span class="text-primary">= </span> <span class="orange">['Ruby on Rails','VueJS','NodeJS(express)','Bootstrap']</span>
       <br> 2.5.7 :011 > <span class="text-success">other_dev_tool </span> <span class="text-primary">= </span> <span class="orange">['Git','Github','Heroku','Linux']</span>
       <br> 2.5.7 :012 > <span class="text-success">exit</span>
-      <br> 2.5.7 :013 > <input type="text" class="text-success console-input w-80" autofocus>
+      <br> <span class="pink">root@hama99o:~$ </span> <input type="text" class="text-success console-input w-80" autofocus>
     </div>
   </div>
 </template>
