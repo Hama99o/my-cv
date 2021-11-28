f24c8d108e947b0c97616dd7b93b4e53f410c329
hama <hmmshl@gmail.com>
2021-11-28 18:58:40 +0100

    console updated

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -3,19 +3,19 @@
     <div class="top" @click="toggleConsole"> <span class="options">⦿ x ○</span> <span class="title">{{ $t('message.console.phrase.consoleHeading') }}</span></div>
     <div v-if="open" class="text">
       <span class="pink">root@hama99o:~$ </span> rails c
-      <br> 2.5.7 :001 > <span class="text-success"> {{ consoleQuestions.myNameAndAGe }} </span> <span class="text-primary">= </span> <span class="orange">'Hammayoun Safi',</span> <span class="text-warning">{{myAge}}</span>
-      <br> 2.5.7 :002 > <span class="text-success">{{ consoleQuestions.from }} </span> <span class="text-primary">= </span> <span class="orange"> {{ consoleAnswers.from }} </span>
-      <br> 2.5.7 :003 > <span class="text-success">{{ consoleQuestions.lived }} </span> <span class="text-primary">= </span> <span class="orange">{{ consoleAnswers.lived }}</span>
-      <br> 2.5.7 :004 > <span class="text-success">{{ consoleQuestions.living }} </span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.living }}</span>
-      <br> 2.5.7 :005 > <span class="text-success">{{ consoleQuestions.myHobbies }}</span> <span class="text-primary">= </span> <span class="orange">{{ consoleAnswers.myHobbies }}</span>
-      <br> 2.5.7 :006 > <span class="text-success">{{ consoleQuestions.languages }} </span> <span class="text-primary">= </span> <span class="orange">{{ consoleAnswers.languages }}</span>
-      <br> 2.5.7 :007 > <span class="text-success">{{ consoleQuestions.programmingLanguages }} </span> <span class="text-primary">= </span> <span class="orange">{{ consoleAnswers.programmingLanguages }}</span>
-      <br> 2.5.7 :008 > <span class="text-success">{{ consoleQuestions.databaseServer }} </span> <span class="text-primary">= </span> <span class="orange">{{ consoleAnswers.databaseServer }}</span>
-      <br> 2.5.7 :009 > <span class="text-success">{{ consoleQuestions.webTechnologies }} </span> <span class="text-primary">= </span> <span class="orange">{{ consoleAnswers.webTechnologies }}</span>
-      <br> 2.5.7 :010 > <span class="text-success">{{ consoleQuestions.frameworks }} </span> <span class="text-primary">= </span> <span class="orange">{{ consoleAnswers.frameworks }}</span>
-      <br> 2.5.7 :011 > <span class="text-success">{{ consoleQuestions.otherDevTool }}</span> <span class="text-primary">= </span> <span class="orange">{{ consoleAnswers.otherDevTool }}</span>
-      <br> 2.5.7 :012 > <span class="text-success">{{ $t('message.console.phrase.exit') }} </span>
-      <br> <span class="pink">root@hama99o:~$ </span> {{ $t('message.console.phrase.cv') }} <router-link :to="{ name: 'HammayounSaficv' }" target="_blank"> CV </router-link>
+      <br> 2.5.7 :001 > <span class="text-success">{{consoleQuestions.myNameAndAGe}} </span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.myNameAndAGe}},</span> <span class="text-warning">{{myAge}}</span>
+      <br> 2.5.7 :002 > <span class="text-success">{{consoleQuestions.from}}</span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.from}}</span>
+      <br> 2.5.7 :003 > <span class="text-success">{{consoleQuestions.lived}}</span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.lived}}</span>
+      <br> 2.5.7 :004 > <span class="text-success">{{consoleQuestions.living}}</span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.living}}</span>
+      <br> 2.5.7 :005 > <span class="text-success">{{consoleQuestions.myHobbies}}</span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.myHobbies}}</span>
+      <br> 2.5.7 :006 > <span class="text-success">{{consoleQuestions.languages}}</span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.languages}}</span>
+      <br> 2.5.7 :007 > <span class="text-success">{{consoleQuestions.programmingLanguages}}</span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.programmingLanguages}}</span>
+      <br> 2.5.7 :008 > <span class="text-success">{{consoleQuestions.databaseServer}}</span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.databaseServer}}</span>
+      <br> 2.5.7 :009 > <span class="text-success">{{consoleQuestions.webTechnologies}}</span> <span class="text-primary">= </span> <span class="orange">{{consoleAnswers.webTechnologies}}</span>
+      <br> 2.5.7 :010 > <span class="text-success">{{consoleQuestions.frameworks}} </span><span class="text-primary">= </span> <span class="orange">{{consoleAnswers.frameworks}}</span>
+      <br> 2.5.7 :011 > <span class="text-success">{{consoleQuestions.otherDevTool}}</span><span class="text-primary">= </span> <span class="orange">{{consoleAnswers.otherDevTool}}</span>
+      <br> 2.5.7 :012 > <span class="text-success">{{$t('message.console.phrase.exit')}}</span>
+      <br> <span class="pink">root@hama99o:~$ </span> {{$t('message.console.phrase.cv')}} <router-link :to="{ name: 'HammayounSaficv' }" target="_blank">CV</router-link>
       <br> <span class="pink">root@hama99o:~$ </span> <input type="text" class="text-success console-input w-80" autofocus>
     </div>
   </div>
