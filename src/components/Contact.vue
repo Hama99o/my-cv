b839e83d7af375b547ab451b13733d44653c1924
hama <hmmshl@gmail.com>
2021-11-30 14:04:16 +0100

    contact updated

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,20 +1,33 @@
 <template>
-  <div>
-    <b-list-group>
-      <div id="contact-form" class="contact-form">
-        <h1 class="contact-form_title" :class="toggleWhiteTextColorForNightMood">Contact Form</h1>
-        <div class="separator"></div>
-
-        <div v-if="isSending" class="loading">Sendig...</div>
-
-        <form class="form" @submit="onSubmit">
-          <input required name="name" v-model='contact.name' placeholder="Name" type="text" autocomplete="off">
-          <input required name="email" v-model="contact.email" placeholder="E-mail" type="email" autocomplete="off">
-          <textarea name="message" v-model="contact.message" rows="4" placeholder="Message"></textarea>
-           <button class="button" :class="toggleWhitebgAndDarkTextClass">Send</button>
-        </form>
+  <div class="resume d-flex justify-content-center" :class="togglebgWithOpacity">
+    <div class="func py-5">
+      <div class="contact" :class="toggleWhiteTextColorForNightMood">
+        <h3 :class="toggleWhiteTextColorForNightMood">Contact</h3>
+        <div class="call my-2"><i class="fa fa-phone"></i><span> +33 7 66 66 62 33</span></div>
+        <div class="address my-2"><i class="fa fa-map-marker"></i><span> Malakoff, France</span></div>
+        <div class="email my-2"><i class="fa fa-envelope"></i><span> hmmshl@gmail.com</span></div>
       </div>
-    </b-list-group>
+      <div class="follow py-3">
+        <h3 :class="toggleWhiteTextColorForNightMood">Follow</h3>
+        <div class="box">
+          <a  href="https://github.com/Hama99o" target="_blank">
+            <i class="fab fa-github" :class="toggleWhitebgAndDarkTextClass"></i>
+          </a>
+          <a  href="https://www.linkedin.com/in/hammayoun-safi-2916581bb" target="_blank">
+            <i class="fab fa-linkedin-in" :class="toggleWhitebgAndDarkTextClass"></i>
+          </a>
+          <a href="https://www.facebook.com/Hmmshl" target="_blank">
+            <i class="fa fa-facebook bg-dark" :class="toggleWhitebgAndDarkTextClass"></i>
+          </a>
+          <a  href="https://www.instagram.com/hama99o" target="_blank">
+          <i class="fab fa-instagram" :class="toggleWhitebgAndDarkTextClass"></i>
+          </a>
+          <a  href="https://twitter.com/hama99o1" target="_blank">
+            <i class="fa fa-twitter" :class="toggleWhitebgAndDarkTextClass"></i>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,111 +38,58 @@ export default {
   name: 'Contact',
   data () {
     return {
-      contact: {
-        name: '',
-        email: '',
-        message: ''
-      },
-      isSending: false
     }
   },
   computed: {
-    ...mapGetters(['toggleWhitebgAndDarkTextClass', 'toggleWhiteTextColorForNightMood'])
+    ...mapGetters(['toggleWhitebgAndDarkTextClass', 'toggleWhiteTextColorForNightMood', 'togglebgWithOpacity'])
   },
   methods: {
-    clearForm () {
-      for (const field in this.contact) {
-        this.contact[field] = ''
-      }
-    },
-    onSubmit (evt) {
-      evt.preventDefault()
-      this.isSending = true
-      // setTimeout(() => {
-      // 	// Build for data
-      // 	let form = new FormData();
-      // 	for (let field in this.contact) {
-      // 		form.append(field, this.contact[field]);
-      // 	}
-      //
-      // 	// Send form to server
-      // 	this.$http.post('/app.php', form).then((response) => {
-      // 		console.log(response);
-      // this.clearForm();
-      // 		this.isSending = false;
-      // 	}).catch((e) => {
-      // 		console.log(e)
-      // 	});
-      //
-      // }, 1000);
-    }
   }
 }
 </script>
 
-<style scoped>
-
-body {
-	background: #f1f1f1;
-	font-family: 'Roboto', sans-serif;
+<style scoped lang="scss">
+.resume {
+  margin: 20px auto;
+  border: 1px solid #bbb;
+  box-shadow: 0 0 20px -3px #bbb;
+  position: relative;
+  display: flex;
 }
 
-.contact-form {
-	font-family: 16px;
-	margin: 0 auto;
-	max-width: 600px;
-	width: 100%;
+.resume .func {
+  padding: 30px 25px;
+}
+.contact div:hover i {
+  color: grey;
 }
 
-.contact-form .separator {
-	border-bottom: solid 1px #ccc;
-	margin-bottom: 15px;
+.follow .box {
+  text-align: center;
+  vertical-align: middle;
 }
 
-.contact-form .form {
-	display: flex;
-	flex-direction: column;
-	font-size: 16px;
+.follow .box a:hover i {
+  background: #e07a5f;
+  border-radius: 5px;
+  transform: rotate(45deg) scale(0.8);
 }
-
-.contact-form_title {
-	color: #333;
-	text-align: left;
-	font-size: 28px;
+.follow .box a:hover i::before {
+  transform: rotate(-45deg) scale(1.5);
 }
-
-.contact-form input[type="email"],
-.contact-form input[type="text"],
-.contact-form textarea {
-	border: solid 1px #e8e8e8;
-	font-family: 'Roboto', sans-serif;
-	padding: 10px 7px;
-	margin-bottom: 15px;
-	outline: none;
+.follow .box i {
+  display: inline-block;
+  font-size: 30px;
+  background: #8d9e78;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  line-height: 60px;
+  color: #fcfcfc;
+  margin: 0 10px 10px 10px;
+  transition-duration: 0.3s;
 }
-
-.contact-form textarea {
-	resize: none;
-}
-
-.contact-form .button {
-	border: solid 1px #da552f;
-	color: white;
-	cursor: pointer;
-	padding: 10px 50px;
-	text-align: center;
-	text-transform: uppercase;
-}
-
-.contact-form .button:hover {
-	border: solid 1px #ea532a;
-}
-
-.contact-form input[type="email"],
-.contact-form input[type="text"],
-.contact-form textarea,
-.contact-form .button {
-	font-size: 15px;
-	border-radius: 3px
+.follow .box i::before {
+  transition-duration: 0.3s;
 }
 </style>
