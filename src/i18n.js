142d9a09260a29cb37f0852b7a6b8b7109164dd3
hama <hmmshl@gmail.com>
2022-01-16 15:25:52 +0100

    navigator.language added

--- src/i18n.js
+++ src/i18n.js
@@ -17,7 +17,7 @@ function loadLocaleMessages () {
 }
 
 export default new VueI18n({
-  locale: localStorage.locale || 'en',
+  locale: !localStorage.locale && navigator.language === 'fr-FR' ? 'fr' : localStorage.locale || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages: loadLocaleMessages()
 })
